4f62b5f3ecda80a8fc37ebd62e6b8525cfacb872
Rudy Brandies <adanb82@gmail.com>
2023-09-26 16:28:22 -0500

    implemented local storage

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
-const dummyTransactions = [
-    { id: 1, text: 'Flower', amount: -20 },
-    { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150}
-];
+// const dummyTransactions = [
+//     { id: 1, text: 'Flower', amount: -20 },
+//     { id: 2, text: 'Salary', amount: 300 },
+//     { id: 3, text: 'Book', amount: -10 },
+//     { id: 4, text: 'Camera', amount: 150}
+// ];
 
-let transactions = dummyTransactions;
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
 function addTransaction(e) {
     e.preventDefault();
@@ -31,6 +32,7 @@ function addTransaction(e) {
         transactions.push(transaction);
         addTransactionDOM(transaction);
         updateValues();
+        updateLocalStorage();
         text.value = '';
         amount.value = '';
     }
@@ -64,16 +66,23 @@ function updateValues() {
         .filter(item => item < 0)
         .reduce((acc, item) => (acc += item), 0) * -1)
         .toFixed(2);
-
+    
     balance.innerText = `$${total}`;
     money_plus.innerText = `$${income}`;
     money_minus.innerText = `$${expense}`;
+    balance.classList.add(total < 0 ? 'minus' : 'plus');
 }
 
-function removeTransaction(id) {
+ function removeTransaction(id) {
     transactions = transactions.filter(transaction => transaction.id !== id);
 
+    updateLocalStorage();
+
     init();
+}
+
+function updateLocalStorage() {
+    localStorage.setItem('transactions', JSON.stringify(transactions));
 }
 
 function init() {
